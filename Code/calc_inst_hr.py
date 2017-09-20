e4d9f01f98a46807af90a2f6ac06a028884cebde
caleb <calebkw94@gmail.com>
2017-09-20 14:54:07 -0400

    added some comments

--- Code/calc_inst_hr.py
+++ Code/calc_inst_hr.py
@@ -12,9 +12,12 @@ def calc_inst_hr(time, voltage):
 
     # indices = peakutils.indexes(voltage, thres = 0.95*np.max(voltage), min_dist = 1000)
 
+    # get sampling rate
     fs = 1 / (time[1] - time[0])
 
+    # choose HRs to search for
     rates = np.arange(40, 200, 20) / 60
+    # find peaks. factor of 10 set kind of arbitrarily
     peaks = scipy.signal.find_peaks_cwt(voltage, fs / rates / 10)
 
     # just take first two peaks to get instantaneous. not that accurate obviously, could give more control
