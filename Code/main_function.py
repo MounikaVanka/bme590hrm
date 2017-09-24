e882d5b3efe31a655928f8614b390255efc43395
caleb <calebkw94@gmail.com>
2017-09-24 15:51:51 -0400

    typos, pep8 in main

--- Code/main_function.py
+++ Code/main_function.py
@@ -2,37 +2,38 @@ from Input_csv_file import *
 from calc_avg_hr import *
 from calc_inst_hr import *
 from Checking_threshold import *
+from Write_output_file import *
 
 
 def main():
 
-    """main funcion return the following output
+    """main function will print the following to file
     1. The avg heart rate
     2. The instantaneous heart rate
-    3. The trace with Tachcardia
+    3. Indication and time of bradycardia or tachycardia
     """
     
-    #Gets the input file named ecg_data.csv
-    [time,voltage]=read_in()
+    # Gets the input file named ecg_data.csv
+    [time, voltage] = read_in()
+
+    # Calculates the Instantaneous Heart Rate
+    bpm_inst_hr = calc_inst_hr(time, voltage)
+
+    # Calculates the Average Heart Rate
+    bpm_avg_hr = calc_avg_hr(time, voltage, window=input("Enter the window time in seconds"))
+
+    a = input("Enter the Bradycardia threshold")
+    b = input("Enter the Tachycardia threshold")
+
+    a = int(a)
+    b = int(b)
     
+    # Checks the Threshold
+    threshold = checking_threshold(a, b, bpm_avg_hr)
 
-    #Calculates the Instantaneous Heart Rate
-    bpm_inst_hr=calc_inst_hr(time,voltage)
-    
-    
-    #Calculates the Avegare Heart Rate
-    bpm_avg_hr=calc_avg_hr(time,voltage,window=input("Enter the window time in seconds"))
+    # Writes to output file
+    write_to_file(bpm_inst_hr, bpm_avg_hr[0], threshold)
 
 
-    a=input("Enter the Bradycarida range")
-    b=input("Enter the Tachycarida range")
-    
-    #Checks the Threshold
-    threshold=checking_threshold(a,b,bmp_avg_hr)
-
-    # Writes to ouput file
-    write_to_file(bmp_inst_hr,bmp_avg_hr,threshold)
-
-
-
-main()
+if __name__ == '__main__':
+    main()
