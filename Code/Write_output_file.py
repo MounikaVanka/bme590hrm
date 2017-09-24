de27b9891b0cd950ed1aa2d6c5acec1b7e538fc6
Tim Hoer <timothy.hoer@duke.edu>
2017-09-24 12:01:13 -0400

    PEP-8 for Write_output_file.py

--- Code/Write_output_file.py
+++ Code/Write_output_file.py
@@ -1,26 +1,19 @@
 import sys
-# from cal_inst_hr import *
-#from Code_1_2 import *
-
-
-global output
 
 
 def write_to_file(a, b, c):
     """ Writes the output to a separate file
 
-    :param a: The instantaneous heart rate is displayed
-    :param b: The avg_heart_rate is displayed
-    :param c: The output string is displayed
+    :param a: instantaneous heart rate
+    :param b: avg_heart_rate
+    :param c: output string
     """
     prev = sys.stdout
     sys.stdout = open("test.txt", "w")
     instantaneous_heart_rate = a
     avg_heart_rate = b
-    Threshold = c
-    print(instantaneous_heart_rate, '\n', avg_heart_rate, '\n', Threshold)
+    threshold = c
+    print(instantaneous_heart_rate, '\n', avg_heart_rate, '\n', threshold)
     sys.stdout.close()
     sys.stdout = prev
 
-#print(output)
-#write_to_file(60, 72, output)
