2e66a2dcff71603f9e866fb334f788452f44aa99
mv105 <mv105@duke.edu>
2017-09-24 22:48:13 -0400

    Modifying the output comment

--- Code/Write_output_file.py
+++ Code/Write_output_file.py
@@ -13,7 +13,7 @@ def write_to_file(a, b, c):
     instantaneous_heart_rate = a
     avg_heart_rate = b
     threshold = c
-    print('Instantaneous Heart Rate is:',instantaneous_heart_rate,'\n','Average Heart Rate is:',avg_heart_rate, '\n','The threshold for each window is"',threshold)
+    print('Instantaneous Heart Rate is:',instantaneous_heart_rate,'\n','Average Heart Rate is:',avg_heart_rate, '\n','The ouput heart condition is"',threshold)
     sys.stdout.close()
     sys.stdout = prev
 
