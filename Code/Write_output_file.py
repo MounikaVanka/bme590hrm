14fed621e9ba87f55b8790e75683b87e41fffb3f
timhoer <timothy.hoer@duke.edu>
2017-09-24 22:55:20 -0400

    Update Write_output_file.py

--- Code/Write_output_file.py
+++ Code/Write_output_file.py
@@ -9,11 +9,11 @@ def write_to_file(a, b, c):
     :param c: output string
     """
     prev = sys.stdout
-    sys.stdout = open("test.txt", "w")
+    sys.stdout = open("output.txt", "w")
     instantaneous_heart_rate = a
     avg_heart_rate = b
     threshold = c
-    print('Instantaneous Heart Rate is:',instantaneous_heart_rate,'\n','Average Heart Rate is:',avg_heart_rate, '\n','The ouput heart condition is"',threshold)
+    print('Instantaneous Heart Rate is:', instantaneous_heart_rate, '\n', 'Average Heart Rate is:', avg_heart_rate,
+          '\n', 'The condition for each window is', threshold)
     sys.stdout.close()
     sys.stdout = prev
-
