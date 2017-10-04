5cf84172b2f2682ac2e19d8eeeb64f7e08037874
MounikaVanka <31496780+MounikaVanka@users.noreply.github.com>
2017-10-04 18:02:23 -0400

    Update Read_input.py

--- Code/Read_input.py
+++ Code/Read_input.py
@@ -2,6 +2,6 @@
 def read_in():
 
     import numpy as np
-    file=numpy.genfromtxt('ecg_data.csv', dtype=['fltvar', 'fltvar'], ... names=['Time', 'Voltage'], delimiter="," )
+    file=numpy.genfromtxt('ecg_data.csv', dtype=['fltvar', 'fltvar'], ... names=['Time', 'Voltage'], delimiter=" , ")
 
 read_in()
