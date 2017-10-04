42b94fde91c65ba5cfddfb9415bb67285235083b
MounikaVanka <31496780+MounikaVanka@users.noreply.github.com>
2017-10-04 17:24:42 -0400

    Updating to pep8 format

--- Code/Read_input.py
+++ Code/Read_input.py
@@ -1,12 +1,7 @@
 
-
-
 def read_in():
 
     import numpy as np
-
-
-    file=numpy.genfromtxt('ecg_data.csv',dtype=['fltvar','fltvar'],... names = ['Time','Voltage'], delimiter=",")
-
+    file=numpy.genfromtxt('ecg_data.csv', dtype=['fltvar', 'fltvar'],... names=['Time', 'Voltage'], delimiter=" , ")
 
 read_in()
