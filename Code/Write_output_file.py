1eca0d9530a017509f6e878c97d294c81a880ab7
caleb <calebkw94@gmail.com>
2017-10-10 18:35:00 -0400

    ecg class now works with defaults from extractable classy main. closes #48

--- Code/Write_output_file.py
+++ Code/Write_output_file.py
@@ -1,6 +1,7 @@
-def write_to_file(inst_hr, avg_hr, output):
-    """Writes the output to a separate file
+def write_to_file(output_filename, inst_hr, avg_hr, output):
+    """ Writes the output to a separate file
 
+    :param: output_filename: str, name for output
     :param inst_hr: int instantaneous heart rate
     :param avg_hr: int avg_heart_rate
     :param output: string output
@@ -9,7 +10,7 @@ def write_to_file(inst_hr, avg_hr, output):
     import sys
     prev = sys.stdout
 
-    sys.stdout = open("output.txt", "w")
+    sys.stdout = open(output_filename, "w")
     print('Instantaneous Heart Rate is:', inst_hr)
     print('Average Heart Rate is:', avg_hr)
     print('The condition for each window is', output)
--- Code/ecg.py
+++ Code/ecg.py
@@ -1,31 +1,35 @@
 class ECG:
 
-    def __init__(self):
+    def __init__(self, file, window, brady_max, tachy_min):
         from Input_csv_file import read_in
         from calc_inst_hr import calc_inst_hr
         import numpy as np
         from Checking_threshold import checking_threshold
         from calc_avg_hr import calc_avg_hr
+
         (self.time, self.voltage) = read_in()
-        self.input_file = input("Please enter the input filename(s): ")
-        a = input("Enter the window time in minutes: ")
-        b = input("Enter the Bradycardia threshold: ")
-        c = input("Enter the Tachycardia threshold: ")
-        self.window = 60 * int(a)
-        self.threshold = (int(b), int(c))
+        self.input_file = file
+        self.window = window
+        self.threshold = (brady_max, tachy_min)
+
         self.avg_hr = calc_avg_hr(self.time, self.voltage, self.window)
         self.inst_hr = calc_inst_hr(self.time, self.voltage)
+
         window_states = np.array([])
         for row in self.avg_hr:
-            window = checking_threshold(self.threshold[0], self.threshold[1], row)
+            window = checking_threshold(self.threshold[0], self.threshold[1],
+                                        row)
             window_states = np.append(window_states, window)
         self.condition = window_states
 
     def write_file(self):
         from Write_output_file import write_to_file
-        output_file = self.input_file.split(".")[1] + "_out"
-        write_to_file(output_file, self.inst_hr, self.avg_hr[0], self.condition)
+        output_file = self.input_file.split(".")[0] + "_out"
+        output_file = '.'.join([output_file, 'csv'])
+        write_to_file(output_file, self.inst_hr, self.avg_hr[0],
+                      self.condition)
 
     def print_results(self):
-        print('Instantaneous Heart Rate is:', self.inst_hr, '\n', 'Average Heart Rate is:', self.avg_hr[0], '\n',
+        print('Instantaneous Heart Rate is:', self.inst_hr, '\n',
+              'Average Heart Rate is:', self.avg_hr[0], '\n',
               'The condition for each window is', self.condition)
