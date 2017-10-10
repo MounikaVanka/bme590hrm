b11a0bcab81edf3463fe6a2f6510be9b857087e5
caleb <calebkw94@gmail.com>
2017-10-10 17:41:11 -0400

    reworked classy_hrm to be extractable main function with defaults

--- Code/classy_hrm.py
+++ Code/classy_hrm.py
@@ -1,6 +1,19 @@
-from ecg import ECG
+def classy_hrm(file='ecg_data.csv', window=20, brady_max=40, tachy_min=100):
+    """Main function to be used with class-based approach
 
-data = ECG()
-data.write_file()
-data.print_results()
+    :param file: str, name of input csv file
+    :param window: int, window size to divide signal into for diagnosis
+    :param brady_max: int, upper threshold for bradycardia
+    :param tachy_min: int, lower threshold for tachycardia
+    :return: none
+    """
+    from ecg import ECG
 
+    data = ECG(file, window, brady_max, tachy_min)
+    data.write_file()
+    data.print_results()
+
+
+if __name__ == '__main__':
+    # if running standalone, just use default parameters
+    classy_hrm()
