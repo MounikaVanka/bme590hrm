db071a918549981ebf1753bd38b6e5044ae9a943
Tim Hoer <timothy.hoer@duke.edu>
2017-10-15 16:06:30 -0400

    adding units to doc string for classy_hrm

--- Code/classy_hrm.py
+++ Code/classy_hrm.py
@@ -2,9 +2,9 @@ def classy_hrm(file='ecg_data1.csv', window=20, brady_max=40, tachy_min=100):
     """Main function to be used with class-based approach
 
     :param file: str, name of input csv file
-    :param window: int, window size in sec to divide signal into for diagnosis
-    :param brady_max: int, upper threshold for bradycardia
-    :param tachy_min: int, lower threshold for tachycardia
+    :param window: int, window size in seconds to divide signal into for diagnosis
+    :param brady_max: int, upper threshold for bradycardia in BPM
+    :param tachy_min: int, lower threshold for tachycardia in BPM
     :return: none
     """
     from ecg import ECG
