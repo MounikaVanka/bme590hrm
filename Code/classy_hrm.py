1c8ddf4d91efd65f48587dc424ae2b4687b02bb6
mv105 <mv105@duke.edu>
2017-10-17 18:31:59 -0400

    changing to pep8

--- Code/classy_hrm.py
+++ Code/classy_hrm.py
@@ -5,7 +5,7 @@ def classy_hrm(file='ecg_data1.csv', window=20, brady_max=40, tachy_min=100):
         <file>_out.txt and are printed to the console.
 
     :param file: str, name of input csv file
-    :param window: int, window size in seconds to divide signal into for diagnosis
+    :param window: int,window size in seconds to divide signal for diagnosis
     :param brady_max: int, upper threshold for bradycardia in BPM
     :param tachy_min: int, lower threshold for tachycardia in BPM
     :return: none
